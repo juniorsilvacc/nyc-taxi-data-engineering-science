233868edf6b641c407b263286db0f029622cb813
juniorsilvacc <juniorsilvacc@hotmail.com>
2026-04-11 14:32:58 -0300

    feat: camada de silver, fazendo a limpeza, tratamento e pré-processamento dos dados e armazenando em .parquet

---
+++ src/jobs/transform_taxi_silver.py
@@ -0,0 +1,75 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+import os
+
+def transform_bronze_to_silver():
+    base_path = os.getcwd()
+    input_path = os.path.join(base_path, "data/bronze/nyc_taxi")
+    output_path = os.path.join(base_path, "data/silver/nyc_taxi")
+
+    spark = SparkSession.builder \
+        .appName("NYC_Taxi_Silver_Transformation") \
+        .config("spark.driver.memory", "8g") \
+        .config("spark.executor.memory", "8g") \
+        .config("spark.sql.shuffle.partitions", "20") \
+        .config("spark.default.parallelism", "10") \
+        .getOrCreate()
+
+    print(f"Lendo dados da Bronze em: {input_path}")
+
+    try:
+        df_bronze = spark.read.parquet(input_path)
+
+        # 1. Padronização de nomes, Tipagem e Tratamento de Nulos...
+        df_silver = df_bronze.select(
+            F.col("VendorID").cast("int").alias("vendor_id"),
+            F.to_timestamp(F.col("tpep_pickup_datetime"), "yyyy-MM-dd HH:mm:ss").alias("pickup_datetime"),
+            F.to_timestamp(F.col("tpep_dropoff_datetime"), "yyyy-MM-dd HH:mm:ss").alias("dropoff_datetime"),
+            F.coalesce(F.col("passenger_count").cast("int"), F.lit(1)).alias("passenger_count"), # Se nulo, assume 1
+            F.col("trip_distance").cast("double").alias("trip_distance"),
+            F.col("fare_amount").cast("double").alias("fare_amount"),
+            F.col("tip_amount").cast("double").alias("tip_amount"),
+            F.col("total_amount").cast("double").alias("total_amount")
+        )
+        
+        # 2. Deduplicação
+        initial_count = df_silver.count()
+        df_silver = df_silver.dropDuplicates(["vendor_id", "pickup_datetime", "dropoff_datetime", "trip_distance"])
+        
+        after_dedup_count = df_silver.count()
+        print(f"Deduplicação concluída. Registros duplicados removidos: {initial_count - after_dedup_count}")
+        
+        # 3. Limpeza de Anomalias (Data Quality)
+        df_silver = df_silver.filter(
+            (F.col("total_amount") > 0) & 
+            (F.col("trip_distance") > 0) &
+            (F.col("pickup_datetime").isNotNull()) &
+            (F.col("dropoff_datetime") > F.col("pickup_datetime"))
+        )
+        
+        # 4. Feature Engineering (Criação de Colunas de Tempo)
+        df_silver = df_silver.withColumn("trip_duration_minutes", 
+            (F.unix_timestamp("dropoff_datetime") - F.unix_timestamp("pickup_datetime")) / 60
+        ).withColumn("hour_of_day", F.hour("pickup_datetime")) \
+         .withColumn("day_of_week", F.dayofweek("pickup_datetime")) \
+         .withColumn("is_weekend", F.when(F.col("day_of_week").isin(1, 7), True).otherwise(False))
+        
+        # 5. Auditoria
+        final_count = df_silver.count()
+        print(f"Limpeza de anomalias concluída. Registros removidos: {after_dedup_count - final_count}")
+        print(f"Total de registros na Silver: {final_count:,}")
+
+        # 6. Salvando na Silver com particionamento
+        print(f"Gravando dados na Silver em: {output_path}")
+        
+        df_silver.coalesce(5).write.mode("overwrite").parquet(output_path)
+        
+        print("Camada Silver concluída com sucesso!")
+
+    except Exception as e:
+        print(f"Erro na transformação Silver: {e}")
+    finally:
+        spark.stop()
+
+if __name__ == "__main__":
+    transform_bronze_to_silver()
